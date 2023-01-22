2e3af1f800e607253e46620e65d481519c5b7715
Vigneshwaran Rajasekaran <123082001+vigneshwaranwh@users.noreply.github.com>
2023-01-22 07:54:20 +0530

    Add files via upload

---
+++ assets/js/paragraph.js
@@ -0,0 +1,13 @@
+const paragraphs = [
+
+    "computer science, the study of computers and computing, including their theoretical and algorithmic foundations, hardware and software, and their uses for processing information. The discipline of computer science includes the study of algorithms and data structures, computer and network design, modeling data and information processes, and artificial intelligence.",
+
+    "Computer science draws some of its foundations from mathematics and engineering and therefore incorporates techniques from areas such as queueing theory, probability and statistics, and electronic circuit design. Computer science also makes heavy use of hypothesis testing and experimentation during the conceptualization, design, measurement, and refinement of new algorithms, information structures, and computer architectures.",
+
+    "Computer science is considered as part of a family of five separate yet interrelated disciplines: computer engineering, computer science, information systems, information technology, and software engineering. This family has come to be known collectively as the discipline of computing. These five disciplines are interrelated in the sense that computing is their object of study, but they are separate since each has its own research perspective and curricular focus.",
+
+    "The major subfields of computer science include the traditional study of computer architecture, programming languages, and software development. However, they also include computational science (the use of algorithmic techniques for modeling scientific data), graphics and visualization, human-computer interaction, databases and information systems, networks, and the social and professional issues that are unique to the practice of computer science.",
+
+    "As may be evident, some of these subfields overlap in their activities with other modern fields, such as bio-informatics and computational chemistry. These overlaps are the consequence of a tendency among computer scientists to recognize and act upon their field’s many interdisciplinary connections."
+    
+];
